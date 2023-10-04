3f7ba71510f60dad672c052c2595e263cafe14c8
yumna El Hendi <elhe0016@algonquinlive.com>
2023-10-04 13:37:25 -0400

    Completes the assignment

--- functions.js
+++ functions.js
@@ -1,10 +1,47 @@
 // List function
 function list(clients) {
-    return clients.map((client, index) => `
-      <li class="list-group-item d-flex justify-content-between" data-index="${index}">
-        <strong>${client.name} - $ ${client.balance.toFixed(2)}</strong>
+    const listItems = clients.map(client => `
+      <li class="list-group-item d-flex justify-content-between" data-index="${client.id}">
+        <span>${client.name}</span>
+        <span>$ ${client.balance.toFixed(2)}</span>
       </li>
-    `).join('');
+    `);
+  
+    return listItems.join('');
+  }
+  //Order function
+
+  function order(clients, property) {
+    clients.sort((a, b) => {
+      if (a[property] < b[property]) {
+        return -1;
+      } else if (a[property] > b[property]) {
+        return 1;
+      }
+      return 0;
+    });
+  
+    return clients;
   }
   
- 
+  // Total function
+  function total(clients) {
+    return clients.reduce((sum, client) => sum + client.balance, 0);
+  }
+  
+  //info function
+  function info(index) {
+    const client = clients.find((client, i) => i === index);
+  
+    return client;
+  }
+
+  //search function
+
+  function search(query) {
+    const results = clients.filter(client => client.name.toLowerCase().includes(query.toLowerCase()));
+    
+    return results;
+  }
+  
+  
